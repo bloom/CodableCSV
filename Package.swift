d97041993be7dd7e850ed2de4344e25cdb80da7a
BJ Homer <bjhomer@gmail.com>
2021-08-25 13:59:02 -0600

    Drop support for older OS versions in Package.swift.
    Xcode 13 is choking when building for release. It's having trouble dealing with Combine headers that are only conditionally available.
    
    We don't care about that in Day One, so I'm making a fork that just ups the deployment target. This seems to fix the issue.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.4
 import PackageDescription
 
 let package = Package(
     name: "CodableCSV",
     platforms: [
-        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_13), .iOS(.v14), .tvOS(.v14), .watchOS(.v6)
     ],
     products: [
         .library(name: "CodableCSV", targets: ["CodableCSV"]),
